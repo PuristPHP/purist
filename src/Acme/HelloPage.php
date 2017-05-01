e39bed6093580600f483d38f3a714dabe613d737
Nicholas Ruunu <nicholas@ruu.nu>
2017-05-02 00:56:06 +0200

    Updates application to new http-messages version

--- src/Acme/HelloPage.php
+++ src/Acme/HelloPage.php
@@ -2,23 +2,22 @@
 
 namespace Acme;
 
-use GuzzleHttp\Psr7\Stream;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Purist\Exception;
-use Purist\Header\HttpHeaders;
+use Purist\Http\Header\HttpHeaders;
+use Purist\Http\Response\TextResponse;
 use Purist\Message;
-use Purist\Response\Response;
 use Purist\Server\Resource;
 
 class HelloPage implements Resource
 {
     public function response(RequestInterface $request): ResponseInterface
     {
-        $stream = new Stream(fopen('php://temp', 'r+'));
-
-        $stream->write('Det gick!');
-        $stream->rewind();
-        return new Response(new Message($stream, new HttpHeaders(['Content-Type' => 'text/html'])));
+        return new TextResponse(
+            'Hello World!',
+            200,
+            new HttpHeaders(['Content-Type' => 'text/html'])
+        );
     }
 }
