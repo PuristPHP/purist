508c54758d63e2d50c4f091803a2269fa7652f98
Nicholas Ruunu <nicholas@ruu.nu>
2018-01-19 12:26:49 +0100

    Make protocol information header first in order

--- src/Purist/Server/ResourceServer.php
+++ src/Purist/Server/ResourceServer.php
@@ -19,12 +19,6 @@ class ResourceServer implements Server
     {
         $response = $this->resource->response($request);
 
-         foreach ($response->getHeaders() as $name => $values) {
-             foreach ($values as $value) {
-                 header(sprintf('%s: %s', $name, $value), false, $response->getStatusCode());
-             }
-         }
-
         header(
             sprintf(
                 'HTTP/%s %s %s',
@@ -36,8 +30,19 @@ class ResourceServer implements Server
             $response->getStatusCode()
         );
 
+        foreach ($response->getHeaders() as $name => $values) {
+            foreach ($values as $value) {
+                header(sprintf('%s: %s', $name, $value), false, $response->getStatusCode());
+            }
+        }
+
         print $response->getBody()->getContents();
 
+        $this->finishRequest();
+    }
+
+    private function finishRequest(): void
+    {
         if (function_exists('fastcgi_finish_request')) {
             fastcgi_finish_request();
         } elseif ('cli' !== PHP_SAPI) {
@@ -45,7 +50,7 @@ class ResourceServer implements Server
         }
     }
 
-    private function cleanOutputBuffers()
+    private function cleanOutputBuffers(): void
     {
         $status = ob_get_status(true);
         $level = count($status);
